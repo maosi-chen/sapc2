c2fe18523baa3b2345c92dc112427c002a1aae8b
unknown <maosi.chen.1984@gmail.com>
2020-09-26 16:02:05 -0700

    Initial commit (data_preprocess_layer_v2)

--- trainer/layers/data_preprocess_layer_v2.py
+++ trainer/layers/data_preprocess_layer_v2.py
@@ -5,6 +5,7 @@ import os, sys
 import numpy as np
 
 import tensorflow as tf
+import tensorflow_probability as tfp
 
 import tensorflow.keras as K
 
@@ -291,6 +292,125 @@ def read_batch_zip_crop_flip_records_train_v4(
     dataset = dataset.shuffle(buffer_size, seed=shuffle_seed, reshuffle_each_iteration=True)
   else:
     dataset = dataset.repeat(num_epochs)
+
+  dataset = dataset.interleave(
+    lambda x: tf.data.TFRecordDataset(
+      x,
+      compression_type=compression_type,
+      num_parallel_reads=1 #num_parallel_reads #1
+    ),
+    cycle_length=num_parallel_reads,
+    block_length=1,
+    num_parallel_calls=tf.data.experimental.AUTOTUNE, #num_parallel_calls #tf.data.experimental.AUTOTUNE
+    deterministic=True
+  )
+
+  # dataset = tf.data.TFRecordDataset(
+  #   filenames,
+  #   compression_type=compression_type,
+  #   num_parallel_reads=num_parallel_reads)
+  # if (buffer_size is not None):
+  #   # dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=buffer_size, count=num_epochs, seed=shuffle_seed))
+  #   # dataset.apply(tf.data.experimental.filter_for_shard(num_workers, worker_index))
+  #   dataset = dataset.shard(num_workers, worker_index)
+  #   dataset = dataset.repeat(num_epochs)
+  #   dataset = dataset.shuffle(buffer_size, seed=shuffle_seed, reshuffle_each_iteration=True)
+  # else:
+  #   dataset = dataset.repeat(num_epochs)
+
+  dataset = dataset.batch(
+    batch_size,
+    drop_remainder=drop_remainder
+  )
+  #dataset = dataset.map(parser_method_single_input, num_parallel_calls=num_parallel_calls)
+  #dataset = dataset.map(_parse_SeqExampleProto_zip_crop_flip_v4, num_parallel_calls=num_parallel_calls)
+
+  #dataset = dataset.map(parser_v4(flt_fs), num_parallel_calls=num_parallel_calls)
+  dataset = dataset.map(parser_method, num_parallel_calls=tf.data.experimental.AUTOTUNE) #num_parallel_calls)
+  #dataset = dataset.cache()
+
+  # dataset = dataset.cache() # cache to memory
+
+  dataset = dataset.prefetch(buffer_size=prefetch_buffer_size)
+
+  return dataset
+
+
+def _filter_cc_fn(flt_fs, cc_threshold_ts, in_example_proto):
+  ## parse a sample
+  features_dict = {
+    'ARD_LST_ref': tf.io.FixedLenFeature(shape=[flt_fs * flt_fs], dtype=tf.float32),
+    'ARD_LST_tgt': tf.io.FixedLenFeature(shape=[flt_fs * flt_fs], dtype=tf.float32)
+  }
+
+  # parse
+  features = tf.io.parse_single_example(
+    in_example_proto,
+    features_dict)
+  # [flt_fs * flt_fs]
+  ARD_LST_ref1D = features["ARD_LST_ref"]
+  ARD_LST_tgt1D = features["ARD_LST_tgt"]
+
+  ## calculate correlation coefficient
+  # []
+  cc = tfp.stats.correlation(ARD_LST_ref1D, ARD_LST_tgt1D, sample_axis=0, event_axis=None)
+
+  ## determine whether the cc is above cc_threshold
+  return tf.cond(
+    tf.math.greater_equal(cc, cc_threshold_ts),
+    lambda: True,
+    lambda: False
+  )
+
+def filter_cc(flt_fs, cc_threshold_ts):
+  def wrapper(in_sample):
+    return _filter_cc_fn(flt_fs, cc_threshold_ts, in_sample)
+  return wrapper
+
+
+def read_batch_zip_crop_flip_records_train_v4_wCFilter(
+  files_pattern,  # filenames,
+  compression_type="GZIP",  # ""
+  batch_size=20,
+  num_epochs=None,
+  buffer_size=50000,
+  shuffle_seed=None,
+  ## new Dataset v1.8 features
+  num_parallel_reads=1,
+  num_parallel_calls=4,  # suggestion: use the number of available CPU cores
+  prefetch_buffer_size=tf.data.experimental.AUTOTUNE,  # 1,
+  ## new Dataset v1.13 features
+  num_workers=1,
+  worker_index=0,
+  drop_remainder=False,
+  #
+  cc_threshold = 0.0, # correlation coefficient threshold (examples with cc above which is allowed in.)
+  #
+  # application specific parameters
+  parser_method=parser_v4(64),  # _parse_SeqExampleProto_zip_crop_flip_v4,
+  flt_fs=64
+  ):
+  # inputs
+  #    'ARD_LST_ref':        tf.TensorShape([B, flt_fs, flt_fs]),
+  #    'ref_DOY':            tf.TensorShape([B]),
+  #    'DOYDiff_TmR':        tf.TensorShape([B]),
+  #    'ARD_LST_tgt_masked': tf.TensorShape([B, flt_fs, flt_fs]),
+  #    'tgt_DOY':            tf.TensorShape([B]),
+  #    'DOYDiff_RmT':        tf.TensorShape([B]),
+  #    'Mask_Img':           tf.TensorShape([B, flt_fs, flt_fs])
+  # label
+  #    'ARD_LST_tgt, Mask_Img': tf.TensorShape([B, flt_fs, flt_fs, 2])
+
+  cc_threshold_ts = tf.convert_to_tensor(cc_threshold, dtype=tf.float32)
+  filter_cc_fn = filter_cc(flt_fs, cc_threshold_ts)
+
+  dataset = tf.data.Dataset.list_files(files_pattern, seed=shuffle_seed + 100)
+  if (buffer_size is not None):
+    dataset = dataset.shard(num_workers, worker_index)
+    dataset = dataset.repeat(num_epochs)
+    dataset = dataset.shuffle(buffer_size, seed=shuffle_seed, reshuffle_each_iteration=True)
+  else:
+    dataset = dataset.repeat(num_epochs)
     
   dataset = dataset.interleave(
     lambda x: tf.data.TFRecordDataset(
@@ -303,7 +423,9 @@ def read_batch_zip_crop_flip_records_train_v4(
     num_parallel_calls=tf.data.experimental.AUTOTUNE,
     deterministic=True
   )
-  
+
+  dataset = dataset.filter(filter_cc_fn)
+
   dataset = dataset.batch(
     batch_size,
     drop_remainder=drop_remainder
@@ -374,7 +496,63 @@ def create_combined_transformed_dataset_v3d1_gs(
   return combined_transformed_dataset_v3
 
 
-### ***** currently USED as of 02/11/2020 ***** ###
+def create_combined_transformed_dataset_v3d1_gs_wCFilter(
+  gs_file_pattern,
+  batch_size,
+  buffer_size,
+  shuffle_seed=42,
+  num_epochs=1,
+  num_parallel_reads=1,
+  num_parallel_calls=2,
+  flt_fs=64,
+  cc_threshold=0.0
+):
+  # files = tf.matching_files("gs://my-bucket/train-data-*.csv")
+  # transformed_file_list = generate_file_list_transformed(transformed_file_dir)
+  transformed_file_list = tf.io.matching_files(gs_file_pattern)
+
+  # read all combined zip_crop_flip tfrecords in transformed_file_list into a datset
+  combined_transformed_dataset_v3 = read_batch_zip_crop_flip_records_train_v3_wCFilter(
+    transformed_file_list,
+    compression_type="GZIP",
+    batch_size=batch_size,  # 20
+    num_epochs=num_epochs,  # 1,
+    buffer_size=buffer_size,
+    shuffle_seed=shuffle_seed,
+    padding_values=(
+      (
+        tf.constant(-9998.0, dtype=tf.float32),
+        tf.constant(-9998.0, dtype=tf.float32),
+        tf.constant(-9998.0, dtype=tf.float32),
+        tf.constant(-9998.0, dtype=tf.float32),
+        tf.constant(-9998.0, dtype=tf.float32),
+        tf.constant(-9998.0, dtype=tf.float32),
+        tf.constant(-9998.0, dtype=tf.float32)
+      ),
+      (
+        tf.constant(-9998.0, dtype=tf.float32),
+        tf.constant(-9998.0, dtype=tf.float32)
+      )
+    ),
+    ## new Dataset v1.8 features
+    num_parallel_reads=num_parallel_reads,  # 1,
+    num_parallel_calls=num_parallel_calls,  # 2, # suggestion: use the number of available CPU cores
+    prefetch_buffer_size=1,
+    ## new Dataset v1.13 features
+    num_workers=1,
+    worker_index=0,
+    drop_remainder=True,  # False,
+    #
+    # application specific parameters
+    parser_method=_parse_SeqExampleProto_zip_crop_flip_v3,
+    flt_fs=flt_fs,
+    cc_threshold=cc_threshold
+  )
+
+  return combined_transformed_dataset_v3
+
+
+### ***** previously USED as of 02/11/2020 ***** ###
 # this version supports finding
 def create_combined_transformed_dataset_v4(
   gs_file_pattern,
@@ -410,6 +588,50 @@ def create_combined_transformed_dataset_v4(
   )
   
   return combined_transformed_dataset_v4
+
+#####
+### ***** currently USED as of 08/19/2020 ***** ###
+# with filtering on correlation coefficient between the target and reference pair.
+def create_combined_transformed_dataset_v4_wCFilter(
+  gs_file_pattern,
+  batch_size,
+  buffer_size,
+  shuffle_seed=42,
+  num_epochs=1,
+  num_parallel_reads=1,
+  num_parallel_calls=2,
+  flt_fs=64,
+  cc_threshold = 0.0
+  ):
+  # files = tf.matching_files("gs://my-bucket/train-data-*.csv")
+  # transformed_file_list = generate_file_list_transformed(transformed_file_dir)
+  # transformed_file_list = tf.io.matching_files(gs_file_pattern)
+
+  # read all combined zip_crop_flip tfrecords in transformed_file_list into a datset
+  combined_transformed_dataset_v4_wCFilter = read_batch_zip_crop_flip_records_train_v4_wCFilter(
+    gs_file_pattern,  # transformed_file_list,
+    compression_type="GZIP",
+    batch_size=batch_size,  # 20
+    num_epochs=num_epochs,  # 1,
+    buffer_size=buffer_size,
+    shuffle_seed=shuffle_seed,
+    ## new Dataset v1.8 features
+    num_parallel_reads=num_parallel_reads,  # 1,
+    num_parallel_calls=num_parallel_calls,  # 2, # suggestion: use the number of available CPU cores
+    prefetch_buffer_size=tf.data.experimental.AUTOTUNE,  # 1,
+    ## new Dataset v1.13 features
+    num_workers=1,
+    worker_index=0,
+    drop_remainder=True,  # False,
+    #
+    cc_threshold = cc_threshold, # correlation coefficient threshold (examples with cc above which is allowed in.)
+    #
+    # application specific parameters
+    parser_method=parser_v4(flt_fs),  # _parse_SeqExampleProto_zip_crop_flip_v4,
+    flt_fs=flt_fs
+  )
+
+  return combined_transformed_dataset_v4_wCFilter
 
 
 ##----------------------------------------------------------------------------------------------##
@@ -619,8 +841,10 @@ class missing_pixel_reconstruct_data_preprocess_layer_v2(K.layers.Layer):
     # split into 3-band ref and tgt images
     # ref_BN_Img:        [B, Used_Img_size, Used_Img_size, 3]
     # tgt_masked_BN_Img: [B, Used_Img_size, Used_Img_size, 3]
-    ref_BN_Img        = tf.stack([ARD_LST_ref,        ref_DOY_Img, DOYDiff_TmR_Img], axis=-1) 
-    tgt_masked_BN_Img = tf.stack([ARD_LST_tgt_masked, tgt_DOY_Img, DOYDiff_RmT_Img], axis=-1) 
+    # ref_masked_BN_Img: [B, Used_Img_size, Used_Img_size, 3]
+    ref_BN_Img        = tf.stack([ARD_LST_ref,        ref_DOY_Img, DOYDiff_TmR_Img], axis=-1)
+    tgt_masked_BN_Img = tf.stack([ARD_LST_tgt_masked, tgt_DOY_Img, DOYDiff_RmT_Img], axis=-1)
+    ref_masked_BN_Img = tf.stack([ARD_LST_ref * Mask_Img, ref_DOY_Img, DOYDiff_TmR_Img], axis=-1)
     
     # adjust mask shape to 4D tensor
     ## [B, Used_Img_size, Used_Img_size, 1]
@@ -637,13 +861,20 @@ class missing_pixel_reconstruct_data_preprocess_layer_v2(K.layers.Layer):
     Mask_Img_3bands = tf.keras.backend.repeat_elements(
       Mask_Img, num_input_channels, self.channel_axis)
 
+    # [B, Used_Img_size, Used_Img_size, 1]
+    tgt_masked_BN_1ly = tf.expand_dims(ARD_LST_tgt_masked, -1)
+
     ### ----------------------------------------------------------------------- ###
     #
     ## ref_BN_Img:        [B, Used_Img_size, Used_Img_size, 3]
     ## tgt_masked_BN_Img: [B, Used_Img_size, Used_Img_size, 3]
+    ## ref_masked_BN_Img: [B, Used_Img_size, Used_Img_size, 3]
     ## Mask_Img:          [B, Used_Img_size, Used_Img_size, 1]
     ## Mask_Img_3bands:   [B, Used_Img_size, Used_Img_size, 3]
-    return [ref_BN_Img, tgt_masked_BN_Img, Mask_Img, Mask_Img_3bands]
+    ## tgt_masked_BN_1ly: [B, Used_Img_size, Used_Img_size, 1]
+    return [ref_BN_Img, tgt_masked_BN_Img, ref_masked_BN_Img, Mask_Img, Mask_Img_3bands,
+            tgt_masked_BN_1ly
+            ]
 
   def get_config(self):
     config = super().get_config()
